eb84cf7f58074e54be16da4462d9b72019f61da9
cliserkad <0_0@cliserkad.xyz>
2024-07-18 19:56:48 -0400

    add system requirements document

--- models/src/Order.java
+++ models/src/Order.java
@@ -1,5 +1,9 @@
 public class Order {
+	long id;
 	Listing listing;
 	User purchaser;
-
+	PhysicalAddress shippingAddress;
+	PhysicalAddress billingAddress;
+	CreditCard paymentMethod;
+	Instant orderDate;
 }
